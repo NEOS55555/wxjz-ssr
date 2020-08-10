513ff93cbd83a9eb1e691819b40fcb210c9128b7
neos55555 <778007921@qq.com>
2020-08-10 14:54:01 +0800

    server

--- server/index.js
+++ server/index.js
@@ -13,17 +13,17 @@ const handle = app.getRequestHandler();
 app.prepare()
   .then(() => {
     const server = express();
-    server.use(session({
+    /*server.use(session({
       name: 'machineCookie',
       secret:"MIIEpAIBAAKCAQEAs4nNSQfaORLb4yL49jcPI+LN+UshKX+gWcqBATwKepN2BvG+",    //设置签名秘钥  内容可以任意填写
       // cookie:{maxAge:30 * 1000 * 60},    //设置cookie的过期时间，例：30minutes后session和相应的cookie失效过期
       // cookie:{maxAge: 20 * 1000},    // 测试用
       resave:false,      //强制保存，如果session没有被修改也要重新保存
       saveUninitialized: false    //如果原先没有session那么久设置，否则不设置
-    }))
+    }))*/
     server.use(cookieParser());
-    server.use(bodyParser.json({limit: '2mb'}));
-    server.use(bodyParser.urlencoded({limit: '2mb', extended:false}));
+    // server.use(bodyParser.json({limit: '2mb'}));
+    // server.use(bodyParser.urlencoded({limit: '2mb', extended:false}));
     // 路由中间件
     server.use('/api', router); // 添加router中间件
 
